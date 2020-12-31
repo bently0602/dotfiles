ef1921fd580565104c0ab03cdfd87db40365179f
v <v>
2020-12-31 06:11:26 +0000

    keymaps

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -54,38 +54,119 @@
 ;; they are implemented.
 
 (map! :leader
-    (:prefix ("d" . "extend")
+    (:prefix ("w" . "extend")
       :desc "focus-up"
-      "d <up>" #'evil-window-up))
+      "<up>" #'evil-window-up))
 
 (map! :leader
-    (:prefix ("d" . "extend")
+    (:prefix ("w" . "extend")
       :desc "focus-down"
-      "d <down>" #'evil-window-down))
+      "<down>" #'evil-window-down))
 
 (map! :leader
-    (:prefix ("d" . "extend")
+    (:prefix ("w" . "extend")
       :desc "focus-left"
-      "d <left>" #'evil-window-left))
+      "<left>" #'evil-window-left))
 
 (map! :leader
-    (:prefix ("d" . "extend")
+    (:prefix ("w" . "extend")
       :desc "focus-right"
-      "d <right>" #'evil-window-right))
+      "<right>" #'evil-window-right))
+
+
+(defun my-vsplit ()
+	""
+	(interactive)
+	(evil-window-split)
+	;;(let ((buffer (generate-new-buffer "untitled")))
+	;;	(switch-to-buffer buffer)
+	;;)
+)
+
+(map! :leader
+    (:prefix ("w" . "extend")
+      :desc "split to bottom"
+      "\"" #'my-vsplit))
+
+(map! :leader
+    (:prefix ("w" . "extend")
+      :desc "split to right"
+      "%" #'evil-window-vsplit))
 
 
 
 (map! :leader
-    (:prefix ("d" . "extend")
+    (:prefix ("w" . "extend")
       :desc "increase-height"
-      "d =" #'evil-window-increase-height))
+      "=" #'evil-window-increase-height))
+
+(map! :leader
+    (:prefix ("w" . "extend")
+      :desc "decrease-height"
+      "-" #'evil-window-decrease-height))
+
+(map! :leader
+    (:prefix ("w" . "extend")
+      :desc "increase-width"
+      "." #'evil-window-increase-width))
+
+(map! :leader
+    (:prefix ("w" . "extend")
+      :desc "decrease-height"
+      "," #'evil-window-decrease-width))
+
+
+(global-set-key (kbd "<M-S-up>") 'shrink-window)
+(global-set-key (kbd "<M-S-down>") 'enlarge-window)
+(global-set-key (kbd "<M-S-left>") 'shrink-window-horizontally)
+(global-set-key (kbd "<M-S-right>") 'enlarge-window-horizontally)
+
+(global-set-key (kbd "<M-up>") 'evil-window-up)
+(global-set-key (kbd "<M-down>") 'evil-window-down)
+(global-set-key (kbd "<M-left>") 'evil-window-left)
+(global-set-key (kbd "<M-right>") 'evil-window-right)
+
+;;(global-set-key (kbd "<C-\">") 'evil-window-split)
+;;(global-set-key (kbd "<C-%>") 'evil-window-vsplit)
+;;(global-set-key (kbd "<C-'>") 'evil-window-split)
+;;(global-set-key (kbd "<C-5>") 'evil-window-vsplit)
+
+(global-set-key (kbd "<C-S-down>") 'evil-window-split)
+(global-set-key (kbd "<C-S-right>") 'evil-window-vsplit)
+
+(global-set-key (kbd "<C-left>") 'beginning-of-line)
+(global-set-key (kbd "<C-right>") 'end-of-line)
+
+;;(global-set-key (kbd "<C-n>") 'evil-buffer-new)
+
+;; # https://stackoverflow.com/questions/24720593/setting-shiftarrow-keys-to-select-text-in-emacs-prelude
+(global-unset-key (vector (list 'shift 'left)))
+(global-unset-key (vector (list 'shift 'right)))
+(global-unset-key (vector (list 'shift 'up)))
+(global-unset-key (vector (list 'shift 'down)))
+(setq prelude-guru nil)
+(setq shift-selection-mode t)
 
 
 
+;; (global-set-key (kbd "C-c +") #'evil-window-increase-height)
+
+;; https://stackoverflow.com/questions/25791605/emacs-how-do-i-create-a-new-empty-buffer-whenever-creating-a-new-frame
+(defun lunaryorn-new-buffer-frame ()
+  "Create a new frame with a new empty buffer."
+  (interactive)
+  (let ((buffer (generate-new-buffer "untitled")))
+    (set-buffer-major-mode buffer)
+    (display-buffer buffer '(display-buffer-pop-up-frame . nil))))
+
+;;(global-set-key (kbd "C-c n") #'lunaryorn-new-buffer-frame)
 
 ;; https://www.emacswiki.org/emacs/NeoTree
-(global-set-key [f8] 'neotree-toggle)
+(global-set-key [f1] 'neotree-toggle)
+(global-set-key [f11] 'neotree)
 
 ;;
-(global-set-key [f9] 'eshell)
+(global-set-key [f2] '+eshell/toggle)
+(global-set-key [f12] '+eshell)
+
 
