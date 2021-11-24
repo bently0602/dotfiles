35c4051e9ccd4b7e5a1725b6a08d37f9166d21eb
vgoff <vgoff@accuregsoftware.com>
2021-11-24 16:36:32 -0600

    update

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # curl -sf -L https://raw.githubusercontent.com/bently0602/dotfiles/main/install.sh | sudo sh
 
 rm -f ~/.tmux.conf
-curl -o ~/.tmux.conf https://raw.githubusercontent.com/bently0602/dotfiles/main/tmux.conf
+curl -s -o ~/.tmux.conf https://raw.githubusercontent.com/bently0602/dotfiles/main/tmux.conf
 
 rm -f ~/.vimrc
-curl -o ~/.vimrc https://raw.githubusercontent.com/bently0602/dotfiles/main/vimrc
+curl -s -o ~/.vimrc https://raw.githubusercontent.com/bently0602/dotfiles/main/vimrc
