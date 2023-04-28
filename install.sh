c53fb6d2fdd7f81b4018c037d257479c9fcaf11f
bently0602 <atx.hx.ag@gmail.com>
2023-04-28 09:38:09 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@ curl -s -o ~/.tmux.conf https://raw.githubusercontent.com/bently0602/dotfiles/ma
 
 rm -f ~/.vimrc
 curl -s -o ~/.vimrc https://raw.githubusercontent.com/bently0602/dotfiles/main/vimrc
+
+rm -f ~/.bashrc
+curl -s -o ~/.bashrc https://raw.githubusercontent.com/bently0602/dotfiles/main/bashrc
+
+tmux source-file ~/.tmux.conf
+source ~/.bashrc
